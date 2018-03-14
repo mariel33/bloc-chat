70736af1d86f7ea5835711da82c72965a0d9faee
Mariel <mariel.capulli@gmail.com>
2018-03-13 20:02:17 -0400

    create chat room feature

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,64 +3,63 @@ import './../App.css';
 
 
 class RoomList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-    
-    this.state = {
-        rooms: [],
-        newRoomName: ''
+
+        this.state = {
+            rooms: [],
+            newRoomName: ''
+        };
+
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+
     };
 
-    this.roomsRef = this.props.firebase.database().ref('rooms');
-
-};
-    
     componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
-        const room = snapshot.val();
-        room.key= snapshot.key;
-        this.setState({ rooms: this.state.rooms.concat( room ) });
-    });
-   
-  }
+        this.roomsRef.on('child_added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat(room) });
+        });
+
+    }
 
     handleChange(e) {
         this.setState({ newRoomName: e.target.value });
-  }
-  
+    }
+
     handleSubmit(e) {
         e.preventDefault();
-        if (!this.state.newRoomName){return}
-        const newRoom ={ name: this.state.newRoomName};
-        this.setState({rooms: [...this.state.rooms, newRoom], newRoomName: ''});
-  }
+        if (!this.state.newRoomName) { return }
+    }
 
-    createRoom(){
+    createRoom() {
         this.roomsRef.push({
             name: this.state.newRoomName
         });
+
     }
 
- 
+
     render() {
         return (
-           <section className ="room-list">
+            <section className="room-list">
                 <div className="side-nav">
                     <ul>
-                        {this.state.rooms.map( (room, index) =>{
-                        return (
-                            <div key={index}>{room.name}</div>
+                        {this.state.rooms.map((room, index) => {
+                            return (
+                                <div key={index}>{room.name}</div>
                             )
                         })}
-                        <form onSubmit={ (e) => this.handleSubmit(e)}>
-                                <input type="text" name="newroom" placeholder="New Room" value={this.state.newRoom} onChange = { (e) => this.handleChange(e) }/>
-                                <button type="submit" onClick={() => this.createRoom()}>Create Room</button>
+                        <form onSubmit={(e) => this.handleSubmit(e)}>
+                            <input type="text" name="newroom" placeholder="New Room" value={this.state.newRoom} onChange={(e) => this.handleChange(e)} />
+                            <button type="submit" onClick={() => this.createRoom()}>Create Room</button>
                         </form>
                     </ul>
                 </div>
-           </section>
+            </section>
         );
-        }
     }
+}
 
-    export default RoomList;
+export default RoomList;
